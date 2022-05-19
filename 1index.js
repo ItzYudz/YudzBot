f03c19ba1fd53a68a8c9c1f25d6ddd9979a85604
ItzYudz <95725567+ItzYudz@users.noreply.github.com>
2022-05-18 21:51:17 -0700

    Create 1index.js

---
+++ 1index.js
@@ -0,0 +1,20 @@
+import {Help} from "./help.js"
+
+import {Something} from "./something.js"
+
+import {Random} from "./random.js"
+
+import {Surprise} from "./surprise.js"
+
+import {Ping} from "./ping.js"
+
+import {RPS} from "./rps.js"
+
+export const Coms = [
+  Help,
+  Something,
+  Random,
+  Surprise,
+  Ping,
+  RPS
+]
